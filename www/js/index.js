f7835b00b56326130652e048458db849363178f8
Makoto Sugano <msugano@google.com>
2020-08-09 22:36:38 +0900

    Simplifying the structure.

--- www/js/index.js
+++ www/js/index.js
@@ -18,34 +18,26 @@
  *
  * (C) Makoto SUGANO, makoto.sugano@gmail.com
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
 
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    app.init();
+}
+
+var app = {
+    init: function() {
 	// domaemon
 	document.addEventListener("offline", this.onOffLine, false);
 	document.addEventListener("backbutton", this.onBackKeyDown, false);
-        $("#mainbutton").on("vclick", app.wikitalkInit);
+	
+	$("#mainbutton").on("vclick", app.actionWikiTalk);
 	$("#summary_mode, #continuous_mode, #random_mode, #voice_speed_slow, #voice_speed_normal, #voice_speed_fast").change(app.settingsChanged);
-
+	
 	// reading the configuration
 	if (window.localStorage.getItem("summaryMode")) {
 	    app.summaryMode = window.localStorage.getItem("summaryMode");
@@ -67,63 +59,99 @@ var app = {
 	    app.voiceSpeed = window.localStorage.getItem("voiceSpeed");
 	    // update the element
 	    switch (app.voiceSpeed) {
-	    case "85":
+	    case "60":
 		console.log("slow");
 		$("#voice_speed_slow").prop("checked", true);
 		break;
-	    case "100":
+	    case "80":
 		console.log("normal");
 		$("#voice_speed_normal").prop("checked", true);
 		break;
-	    case "115":
+	    case "100":
 		console.log("fast");
 		$("#voice_speed_fast").prop("checked", true);
-		break;
+	    break;
 	    default:
 		console.log("not recognized");
 	    }
 	}
-
-	// initial checking of the network connection
-	if (app.checkConnection()) {
-            // navigator.tts.startup(app.ttsStartupInitWin, app.fail);
-	    app.resetHandling("Welcome, press the above image.");
-	} else {
-	    app.resetHandling("Connect to network and try again.");
-	    // navigator.notification.beep(1); onOffLine shall take care.
-	}
 	
-	window.plugins.speechRecognition.getSupportedLanguages(
-	    function(languages){
-		// display the json array
-		console.log(languages);
-		navigator.splashscreen.hide();
-	    },
-	    function(error){
-		console.log("Could not retrieve the languages : " + error);
+	// UPDATE STATE
+	app.state = app.checkConnection();
+    
+	if (app.state == "NO_CONNECTION_STATE") {
+	    app.showInitScreen("Connect to network and try again.");
+	} else {
+	    app.showInitScreen("Welcome, press the above image.");
+	}
+    },
+    showInitScreen: function(message) {
+	console.log("showInitScreen");
+	
+	$("#mainbutton img").attr("src", "img/Wikipedia_logo_silver.png");
+	$("#mainbutton").button();
+	$("#wikitalk_console").text(message);
+
+	// domaemon, enable the settings
+	$("#settings_button").removeClass('ui-disabled');
+    },
+    showSpeakScreen: function(title) {
+	console.log("showSpeakScreen");
+
+	$("#mainbutton img").attr("src", "img/Wikipedia_logo_blue.png");
+	$("#mainbutton").button();
+	$("#wikitalk_console").text(title);
+
+	// domaemon, disable the settings
+	$("#settings_button").addClass('ui-disabled');
+    },
+    actionWikiTalk: function() {
+	console.log("actionWikiTalk");
+	console.log(app.state);
+
+	switch(app.state) {
+	case "WAIT_STATE":
+	    app.state = app.checkConnection();
+	    
+	    if (app.state == "NO_CONNECTION_STATE") {
+		app.showInitScreen("Connect to network and try again.");
+	    } else {
+		app.state = "PLAY_STATE";
+		$.mobile.loading( "show", { text: 'Fetching...', textVisible: true, theme: 'b' } );
+		app.fetchAndReadArticle();
 	    }
-	);
-        console.log('Received Event: ' + id);
+	    break;
+	case "PLAY_STATE":
+	    app.state = "WAIT_STATE";
+	    app.showInitScreen("Stop.");
+	    navigator.notification.beep(1);	    
+	    break;
+	default:
+	    app.state = "WAIT_STATE";
+	    app.showInitScreen("");
+	    break;
+	}
     },
-    fail: function(result) {
-	console.log(JSON.stringify(result));
+    fetchAndReadArticle: function() {
+	app.fetchArticle1A();
     },
-    ttsSpeakWin: function(result) {
-	console.log("ttsSpeakWin");
-	console.log(JSON.stringify(result));
-	app.readParagraph();
+    fetchArticle1A: function() { // Fetching Random Article Titles
+	console.log("fetchArticle1A");
+	
+	app.queryData = {
+	    "action": "query", // query
+	    "list": "random", // random mode
+	    "format": "json", // json
+	    "rnnamespace": 0, // https://en.wikipedia.org/wiki/Wikipedia:Namespace
+	    "rnlimit": 1
+	};
+	
+	// http://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0&rnlimit=1
+	$.getJSON(app.queryUrl, app.queryData, app.fetchArticle2);
     },
-    ttsSpeakWin: function() {
-	console.log("ttsSpeakWin");
-	app.readParagraph();
-    },
-    ttsSpeedWin: function(result) {
-	console.log("ttsSpeedWin");
-	console.log(JSON.stringify(result));
-    },
-    searchNextPage: function() {
-	console.log("searchNextPage");
-	app.queryUrl = 'http://en.wikipedia.org/w/api.php';
+    fetchArticle1B: function(data) { // Fetching Related Article from Titles (Continuous)
+	console.log("fetchArticle1B");
+
 	app.queryData = {
 	    "action": "query", // query
 	    "prop": "links", // links
@@ -134,238 +162,94 @@ var app = {
 	    "indexpageids": "" // returns pageids section for conveinience
 	};
 
-	// http://en.wikipedia.org/w/api.php?action=query&prop=links&format=json&plnamespace=0&pllimit=500&titles=Kyoto&indexpageids=
-
-	$.mobile.loading( "show", { text: 'Fetching...', textVisible: true, theme: 'b' } );
-	$.getJSON(app.queryUrl, app.queryData, app.getJsonRelatedLinks);
+	// http://en.wikipedia.org/w/api.php?action=query&prop=iwlinks&format=json&plnamespace=0&pllimit=500&titles=Kyoto&indexpageids
+	$.getJSON(app.queryUrl, app.queryData, app.fetchArticle2);
     },
-    readParagraph: function() {
-	var paragraphMax = 1;
+    fetchArticle2: function(data) { // Fetching Content from the Title
+	console.log("fetchArticle2");
 
-	if (app.summaryMode == "off") {
-	    paragraphMax = app.paragraphList.length;
-	} else if (app.summaryMode == "on") {
-	    paragraphMax = 1;
-	}
-
-	if (app.paragraphId < paragraphMax) {
-	    app.content = jQuery(app.paragraphList[app.paragraphId]).text();
-	    while (app.content != (app.content = app.content.replace(/\([^\(\)]*\)/g, "")));
-	    while (app.content != (app.content = app.content.replace(/\[[^\[\]]*\]/g, "")));
-	    // not the right place, but for now here comes the speed setting.
-	    // [OLD] navigator.tts.speed(app.voiceSpeed, app.ttsSpeedWin, app.fail);
-	    // [OLD] navigator.tts.speak(app.content, app.ttsSpeakWin, app.fail);
-	    TTS.speak({
-		text: app.content,
-		locale: 'en-GB',
-		rate: app.voiceSpeed / 100
-            }, function () {
-		console.log("ttsSpeakWin");
-		app.ttsSpeakWin();
-            }, function (reason) {
-		app.fail();
-            });
+	if (typeof data.query.random != "undefined") { // from 1A
+	    app.title = data.query.random[0].title;
+	} else { // from 1B
+	    var pageId = data.query.pageids[0];
 	    
-	    app.paragraphId += 1;
-	} else {
-	    app.paragraphId = 0;
-	    if (app.continuousMode == "on") {
-		app.searchNextPage();
-	    } else {
-		app.resetHandling("Finished reading an article.");
-		navigator.notification.beep(1);	    
-	    }
-	}
-    },
-    resetHandling: function(message) {
-	if (app.status == "PLAY_STATE") {
-	    app.ttsStop();
-	    app.status = "READY_STATE";
-	}
-	$("#mainbutton img").attr("src", "img/Wikipedia_logo_silver.png");
-	$("#mainbutton").button();
-	$("#wikitalk_console").text(message);
-	$("#settings_button").removeClass('ui-disabled');
-    },
-    getJsonParagraphs: function(data) {
-	console.log("getJsonParagraphs");
-	var pageId = data.query.pageids[0];
-
-	$.mobile.loading( "hide" );
-
-	if (app.status != "PLAY_STATE") {
-	    return;
-	}
-
-	if (pageId == -1) {
-	    app.resetHandling("The page does not exist: " + app.title);
-	    navigator.notification.beep(1);	    
-	} else {
-	    // update the article title
-	    $("#wikitalk_console").text(app.title);
-	    app.paragraphList = $('<div>' + data.query.pages[pageId].extract + '</div>').children('p');
-	    app.paragraphId = 0;
-	    app.readParagraph();
-	}
-    },
-    getJsonRelatedLinks: function(data) {
-	console.log("getJsonRelatedLinks");
-	console.log(data);
-	var pageId = data.query.pageids[0];
-
-	$.mobile.loading( "hide" );
-
-	if (pageId == -1) {
-	    app.resetHandling("Link does not exist.");
-	    navigator.notification.beep(1);	    
-	} else {
 	    // using random number
 	    // this should improve, deadlinks are also counted here. domaemon.
 	    var pageLength = data.query.pages[pageId].links.length;
 	    app.title = data.query.pages[pageId].links[Math.floor(Math.random()*pageLength)].title;
-	    app.queryUrl = 'http://en.wikipedia.org/w/api.php';
-	    app.queryData = {
-		"action": "query",
-		"prop": "extracts",
-		"format": "json",
-		"exlimit": 10,
-		"exsectionformat": "plain",
-		"titles": app.title,
-		"indexpageids": ""
-	    };
-	    // http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exlimit=10&titles=Kyoto&indexpageids=
-
-	    $.mobile.loading( "show", { text: 'Fetching...', textVisible: true, theme: 'b' } );
-	    $.getJSON(app.queryUrl, app.queryData, app.getJsonParagraphs);
 	}
+
+	var summary = true;
+
+	if (app.summaryMode == "on") {
+	    summary = true;
+	} else if (app.summaryMode == "off") {
+	    summary = false;
+	}
+
+	app.queryData = {
+	    "action": "query",
+	    "prop": "extracts",
+	    "format": "json",
+	    "exlimit": 10,
+	    "exintro": summary, //
+	    "explaintext": true,
+	    "exsectionformat": "wiki",
+	    "titles": app.title,
+	    "indexpageids": true
+	};
+
+	// http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&explaintext=true&exsectionformat=wiki&titles=Kyoto
+	$.getJSON(app.queryUrl, app.queryData, app.readArticle1);
     },
-    // Extract contents from the specific titles. Can include multiple articles.
-    getJsonRandomLinks: function(data) {
-	console.log("getJsonRandomLinks");
+    readArticle1: function(data) { // prep for reading multi paragraphs.
+	console.log("readArticle1");
+	// console.log(JSON.stringify(data));
 
 	$.mobile.loading( "hide" );
-
-	app.title = data.query.random[0].title;
-	console.log(app.title);
-	    app.queryUrl = 'http://en.wikipedia.org/w/api.php';
-	    app.queryData = {
-		"action": "query",
-		"prop": "extracts",
-		"format": "json",
-		"exlimit": 10,
-		"exsectionformat": "plain",
-		"titles": app.title,
-		"indexpageids": ""
-	    };
-
-	$.mobile.loading( "show", { text: 'Fetching...', textVisible: true, theme: 'b' } );
-	$.getJSON(app.queryUrl, app.queryData, app.getJsonParagraphs);
-    },
-    ttsStartupInitWin: function(result) {
-	console.log("ttsStartupInitWin");
-	// navigator.tts.setLanguage("en-US", function(result) { console.log(result); }, app.fail);
-
-	app.status = "READY_STATE";
-    },
-    ttsStartupHalfway: function() {
-	console.log("ttsStartupHalfway");
-	// navigator.tts.setLanguage("en-US", function(result) { console.log(result); }, app.fail);
-
-	// now, continue to the normal procedure.
-	app.status = "READY_STATE";
-	app.wikitalkInit();
 	
-    },
-    ttsStopWin: function(result) {
-	console.log("ttsStopWin");
-    },
-    ttsStop: function() {
-	console.log("ttsStop");
-        // [OLD] navigator.tts.stop(app.ttsStopWin, app.fail);
-	TTS.speak('Stop Reading.', function () {
-	    app.ttsStopWin;
-	}, function (reason) {
-	    app.fail;
-	});
-    },
-    recognizeSpeech: function() {
-	console.log("recognizeSpeech");
-	var maxMatches = 1;
-	var promptString = "Speak Now"; // optional
-	var language = "en-US";         // optional
-	
-	navigator.SpeechRecognizer.startRecognize(function(result){
-            // alert(result);
-	    console.log(result[0]);
-	    // remove the rubbish, must be a bug on the Google side.
-	    app.title = result[0].replace(/[\(\)]/g, "");
-	    app.queryUrl = 'http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exlimit=10&exsectionformat=plain&indexpageids=';
-	    app.queryData = {titles: app.title};
+	var pageId = data.query.pageids[0];
 
-	    $.mobile.loading( "show", { text: 'Fetching...', textVisible: true, theme: 'b' } );
-	    $.getJSON(app.queryUrl, app.queryData, app.getJsonParagraphs);
-	}, function(errorMessage){
-            console.log("Error message: " + errorMessage);
-	    app.resetHandling("Speech not recognized.");
-	}, maxMatches, promptString, language);
-    },
-    wikitalkInit: function() {
-	console.log("wikitalkInit");
+	if (pageId == -1) {
+	    navigator.notification.beep(1);
+	    app.showInitScreen("The page does not exist: " + app.title);
 
-	app.status = app.checkStatus();
-	
-	
-	
-
-	if (!app.checkConnection()) {
-	    app.status = "NOT_READY_STATE";
-	    app.resetHandling("Connect to network and try again.");
-	    navigator.notification.beep(1);	    
-	} else {
-	    if (app.status == "PLAY_STATE") {
-		app.resetHandling("Stop.");
-		navigator.notification.beep(1);	    
-	    } else if (app.status == "READY_STATE") {
-		$("#mainbutton img").attr("src", "img/Wikipedia_logo_blue.png");
-		$("#mainbutton").button();
-		app.status = "PLAY_STATE";
-		// domaemon, disable the settings
-		$("#settings_button").addClass('ui-disabled');
-
-		if (app.randomMode == "on") {
-		    // alert("Random Mode");
-		    app.queryUrl = 'http://en.wikipedia.org/w/api.php';
-
-		    app.queryData = {
-			"action": "query", // query
-			"list": "random", // random mode
-			"format": "json", // json
-			"rnnamespace": 0, // https://en.wikipedia.org/wiki/Wikipedia:Namespace
-			"rnlimit": 1
-		    };
-
-		    // http://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0&rnlimit=1
-
-		    $.mobile.loading( "show", { text: 'Fetching...', textVisible: true, theme: 'b' } );
-		    $.getJSON(app.queryUrl, {}, app.getJsonRandomLinks);
-		} else if (app.randomMode == "off") {
-		    app.recognizeSpeech();
-		}
-	    } else if (app.status == "NOT_READY_STATE") {
-		// should start the engine and comeback here with 
-		// 1) ttsStartup
-		// 2) comeback here.
-		app.ttsStartupHalfway();
-	    }
+	    return;
 	}
+	
+	app.content = data.query.pages[pageId].extract;
+	console.log(app.content);
+
+	app.showSpeakScreen(app.title);
+
+	TTS.speak({
+	    text: app.content,
+	    locale: 'en-GB',
+	    rate: app.voiceSpeed / 100 // devided by 100 - defaul value 80/100
+        }, function () {
+	    app.readArticle2(); // reading ended.
+        }, function (reason) {
+	    app.fail(reason);
+        });	
+    },
+    readArticle2: function() { // reading finished.
+	if (app.continuousMode == "on") {
+	    app.fetchArticle1B();
+	} else {
+	    navigator.notification.beep(1);	    
+	    app.showInitScreen("Finished reading an article.");
+	}
+    },
+    fail: function(reason) {
+	console.log(JSON.stringify(reason));
     },
     checkConnection: function() {
 	var networkState = navigator.connection.type;
 	
 	if (networkState == Connection.NONE) {
-	    return false;
+	    return "NO_CONNECTION_STATE";
 	} else {
-	    return true;
+	    return "WAIT_STATE";
 	}
     },
     settingsChanged: function() {
@@ -396,7 +280,7 @@ var app = {
 	navigator.notification.confirm("Quit WikiTalk?", app.onQuit, "WikiTalk")
     },
     onOutOfNetwork: function() {
-	app.resetHandling("Connect to network and try again.");
+	app.showInitScreen("Connect to network and try again.");
     },
     onOffLine: function() {
 	navigator.notification.beep(1);	    
@@ -409,16 +293,14 @@ var app = {
 	);
     },
     content: null,
-    queryUrl: null,
+    queryUrl: 'http://en.wikipedia.org/w/api.php',
     queryData: null,
-    paragraphId: 0,
-    paragraphList: [],
     title: "Kyoto",
-    status: "NOT_READY_STATE",
+    status: "NO_CONNECTION_STATE",
     randomMode: "on",
     summaryMode: "on",
     continuousMode: "on",
-    voiceSpeed: "100"
+    voiceSpeed: "80"
 };
 
 $('#wikitalk_console').focus(function() {
