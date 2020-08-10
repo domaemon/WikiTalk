84348a57df3620fca8607004d0f51ec13b0f2f41
Makoto Sugano <msugano@google.com>
2020-08-10 13:48:06 +0900

    Adding lang support.

--- www/js/index.js
+++ www/js/index.js
@@ -36,14 +36,43 @@ var app = {
 	document.addEventListener("backbutton", this.onBackKeyDown, false);
 	
 	$("#mainbutton").on("vclick", app.actionWikiTalk);
-	$("#summary_mode, #continuous_mode, #random_mode, #voice_speed_slow, #voice_speed_normal, #voice_speed_fast").change(app.settingsChanged);
-	
+	$("#lang_mode, #continuous_mode, #random_mode, #voice_speed_slow, #voice_speed_normal, #voice_speed_fast").change(app.settingsChanged);
+
+	if (window.localStorage.getItem("langMode")) {
+            console.log(app.langMode);
+	    
+            app.langMode = window.localStorage.getItem("langMode");
+            $('#lang_mode').val(app.langMode);
+	    app.queryUrl = 'http://' + app.langMode + '.wikipedia.org/w/api.php';
+
+	    switch (app.langMode) {
+	    case "en":
+		app.locale = "en-GB";
+		break;
+	    case "zh":
+		app.locale = "zh-CN";
+		break;
+	    case "de":
+		app.locale = "de-DE";
+		break;
+	    case "fi":
+		app.locale = "fi-FI";
+		break;
+	    case "fr":
+		app.locale = "fr-FR";
+		break;
+	    case "ja":
+		app.locale = "ja-JP";
+		break;
+	    case "kr":
+		app.locale = "kr-KR";
+		break;
+	    default:
+		break;
+	    }
+	    console.log(app.langMode);
+	}	
 	// reading the configuration
-	if (window.localStorage.getItem("summaryMode")) {
-	    app.summaryMode = window.localStorage.getItem("summaryMode");
-	    $('#summary_mode').val(app.summaryMode);
-	    console.log(app.summaryMode);
-	}
 	if (window.localStorage.getItem("continuousMode")) {
 	    app.continuousMode = window.localStorage.getItem("continuousMode");
 	    $('#continuous_mode').val(app.continuousMode);
@@ -55,19 +84,18 @@ var app = {
 	    console.log(app.randomMode);
 	}
 	if (window.localStorage.getItem("voiceSpeed")) {
-	    console.log("voiceSpeed changed");
 	    app.voiceSpeed = window.localStorage.getItem("voiceSpeed");
 	    // update the element
 	    switch (app.voiceSpeed) {
-	    case "60":
+	    case app.VOICE_SPEED_SLOW:
 		console.log("slow");
 		$("#voice_speed_slow").prop("checked", true);
 		break;
-	    case "80":
+	    case app.VOICE_SPEED_NORMAL:
 		console.log("normal");
 		$("#voice_speed_normal").prop("checked", true);
 		break;
-	    case "100":
+	    case app.VOICE_SPEED_FAST:
 		console.log("fast");
 		$("#voice_speed_fast").prop("checked", true);
 	    break;
@@ -82,7 +110,7 @@ var app = {
 	if (app.state == "NO_CONNECTION_STATE") {
 	    app.showInitScreen("Connect to network and try again.");
 	} else {
-	    app.showInitScreen("Welcome, press the above image.");
+	    app.showInitScreen("Welcome, press the above image to start the Talk.");
 	}
     },
     showInitScreen: function(message) {
@@ -94,13 +122,25 @@ var app = {
 
 	// domaemon, enable the settings
 	$("#settings_button").removeClass('ui-disabled');
+
+	$("#wikitalk_console_container").show();
+	$("#wikitalk_title_container").hide(); // app.title
+	$("#wikitalk_content_container").hide(); // app.content
+	
     },
-    showSpeakScreen: function(title) {
+    showSpeakScreen: function(title, content) {
 	console.log("showSpeakScreen");
+	
 
 	$("#mainbutton img").attr("src", "img/Wikipedia_logo_blue.png");
 	$("#mainbutton").button();
-	$("#wikitalk_console").text(title);
+	$("#wikitalk_console").text("PLAY_STATE"); // PLAY_STATE
+
+	$("#wikitalk_console_container").hide();
+	$("#wikitalk_title").text(title); // app.title
+	$("#wikitalk_content").text(content); // app.content
+	$("#wikitalk_title_container").show(); // app.title
+	$("#wikitalk_content_container").show(); // app.content
 
 	// domaemon, disable the settings
 	$("#settings_button").addClass('ui-disabled');
@@ -123,12 +163,21 @@ var app = {
 	    break;
 	case "PLAY_STATE":
 	    app.state = "WAIT_STATE";
-	    app.showInitScreen("Stop.");
-	    navigator.notification.beep(1);	    
+
+	    TTS.speak({
+		text: ""
+            }, function () {
+	    }, function (reason) {
+		app.fail(reason);
+	    });	
+	    
+	    navigator.notification.beep(1);
+	    app.showInitScreen("Finished reading an article.");
+
 	    break;
 	default:
 	    app.state = "WAIT_STATE";
-	    app.showInitScreen("");
+	    app.showInitScreen("Unknown Error");
 	    break;
 	}
     },
@@ -179,27 +228,21 @@ var app = {
 	    app.title = data.query.pages[pageId].links[Math.floor(Math.random()*pageLength)].title;
 	}
 
-	var summary = true;
-
-	if (app.summaryMode == "on") {
-	    summary = true;
-	} else if (app.summaryMode == "off") {
-	    summary = false;
-	}
-
 	app.queryData = {
 	    "action": "query",
 	    "prop": "extracts",
 	    "format": "json",
-	    "exlimit": 10,
-	    "exintro": summary, //
+	    "exlimit": "max",
 	    "explaintext": true,
-	    "exsectionformat": "wiki",
+	    "exsectionformat": "plain",
+	    "exintro": true,
 	    "titles": app.title,
 	    "indexpageids": true
 	};
 
-	// http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&explaintext=true&exsectionformat=wiki&titles=Kyoto
+	console.log(app.queryData);
+
+	// http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&explaintext=true&exsectionformat=wiki&titles=Kyoto&exintro=false
 	$.getJSON(app.queryUrl, app.queryData, app.readArticle1);
     },
     readArticle1: function(data) { // prep for reading multi paragraphs.
@@ -220,11 +263,11 @@ var app = {
 	app.content = data.query.pages[pageId].extract;
 	console.log(app.content);
 
-	app.showSpeakScreen(app.title);
+	app.showSpeakScreen(app.title, app.content);
 
 	TTS.speak({
 	    text: app.content,
-	    locale: 'en-GB',
+	    locale: app.langMode,
 	    rate: app.voiceSpeed / 100 // devided by 100 - defaul value 80/100
         }, function () {
 	    app.readArticle2(); // reading ended.
@@ -236,8 +279,7 @@ var app = {
 	if (app.continuousMode == "on") {
 	    app.fetchArticle1B();
 	} else {
-	    navigator.notification.beep(1);	    
-	    app.showInitScreen("Finished reading an article.");
+	    app.actionWikiTalk(); // PLAY_STATE to WAIT_STATE
 	}
     },
     fail: function(reason) {
@@ -255,19 +297,21 @@ var app = {
     settingsChanged: function() {
 	console.log("settingsChanged");
 
-	app.summaryMode = $('#summary_mode').val();
-	window.localStorage.setItem("summaryMode", app.summaryMode);
+	app.langMode = $('#lang_mode').val();
+	window.localStorage.setItem("langMode", app.langMode);
+
 	app.continuousMode = $('#continuous_mode').val();
 	window.localStorage.setItem("continuousMode", app.continuousMode);
+
 	app.randomMode = $('#random_mode').val();
 	window.localStorage.setItem("randomMode", app.randomMode);
 
         if ($("#voice_speed_slow").is(":checked")) {
-	    app.voiceSpeed = "60";
+	    app.voiceSpeed = app.VOICE_SPEED_SLOW;
         } else if ($("#voice_speed_normal").is(":checked")) {
-	    app.voiceSpeed = "80";
+	    app.voiceSpeed = app.VOICE_SPEED_NORMAL;
 	} else if ($("#voice_speed_fast").is(":checked")) {
-	    app.voiceSpeed = "100";
+	    app.voiceSpeed = app.VOICE_SPEED_FAST;
 	}
 	window.localStorage.setItem("voiceSpeed", app.voiceSpeed);
     },
@@ -297,10 +341,14 @@ var app = {
     queryData: null,
     title: "Kyoto",
     status: "NO_CONNECTION_STATE",
+    langMode: "en",
+    locale: "en_GB",
     randomMode: "on",
-    summaryMode: "on",
     continuousMode: "on",
-    voiceSpeed: "80"
+    VOICE_SPEED_SLOW: "80",
+    VOICE_SPEED_NORMAL: "90",
+    VOICE_SPEED_FAST: "100",
+    voiceSpeed: 90
 };
 
 $('#wikitalk_console').focus(function() {
